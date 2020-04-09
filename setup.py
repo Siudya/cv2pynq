e492ce5ca3f16ddc2ffa45a19bcdafca363cbfef
Siudya <32456796+Siudya@users.noreply.github.com>
2020-04-09 16:51:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import os
 import shutil
 import cv2pynq
 
-if 'BOARD' not in os.environ or os.environ['BOARD'] != 'Pynq-Z1':
-    print("Only supported on a Pynq Z1 Board")
+if 'BOARD' not in os.environ or os.environ['BOARD'] != 'Pynq-Z1' or os.environ['BOARD'] != 'Pynq-Z2':
+    print("Only supported on a Pynq Z1 Board or Pynq Z2 Board")
     exit(1)
 
 # Notebook copy
